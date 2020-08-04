0a474c66f995d16d76fa2241240c8bb3884a187f
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-08-04 19:51:36 -0300

    Aula 02 finalizada

--- server/src/database/migrations/03_create_connections.ts
+++ server/src/database/migrations/03_create_connections.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex){
     return knex.schema.createTable('connections', table => {
         table.increments('id').primary()
         table.integer('user_id').notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
-        table.timestamp('created_at').defaultTo('now()').notNullable()
+        table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP')).notNullable()
     })
 }
 export async function down(knex: Knex){
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,18 @@
 import express from 'express'
 
+import ClassesControler from './Controllers/ClassesController'
+import ConnectionsController from './Controllers/ConnectionsController'
+
 const routes = express.Router()
 
-routes.post('/classes', )
+const classesController = new ClassesControler()
+const connectionsController = new ConnectionsController()
+
+routes.post('/classes', classesController.create)
+routes.post('/connections', connectionsController.create)
+
+routes.get('/classes', classesController.index)
+routes.get('/connections', connectionsController.index)
+
 
 export default routes
