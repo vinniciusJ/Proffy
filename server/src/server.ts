e19fc4d4e0740bae7eea28a1f579c3014b79f0da
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-08-04 18:48:29 -0300

    Add controllers

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(routes)
 
-app.post('/', (request, response) => {
-    return response.json({page: 'Home'})
-})
 
 app.listen(3333)
